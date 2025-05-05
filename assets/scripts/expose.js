bd3933272e83f9c089cdba17d090b58f554bed6b
yuankaiy <yuy080@ucsd.edu>
2025-05-04 19:31:17 -0700

    Fixed spacing issues in expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -16,55 +16,55 @@ soundButton.addEventListener("click", playSound);
 }
 
 function changeImg(){
-const selectHorn = document.getElementById('horn-select');
-const image = document.querySelector('#expose img');
-if(selectHorn.value == "air-horn"){
-  image.src = "assets/images/air-horn.svg"
-  image.alt = 'Air Horn'
-}
-else if(selectHorn.value == "car-horn"){
-  image.src = "assets/images/car-horn.svg"
-  image.alt = 'Car Horn'
-}
-else if(selectHorn.value == "party-horn"){
-  image.src = "assets/images/party-horn.svg"
-  image.alt = 'Party Horn'
-}
-else{
-  image.src = 'assets/images/no-image.png';
-  image.alt = 'No image selected';
-}
+  const selectHorn = document.getElementById('horn-select');
+  const image = document.querySelector('#expose img');
+  if(selectHorn.value == "air-horn"){
+    image.src = "assets/images/air-horn.svg"
+    image.alt = 'Air Horn'
+  }
+  else if(selectHorn.value == "car-horn"){
+    image.src = "assets/images/car-horn.svg"
+    image.alt = 'Car Horn'
+  }
+  else if(selectHorn.value == "party-horn"){
+    image.src = "assets/images/party-horn.svg"
+    image.alt = 'Party Horn'
+  }
+  else{
+    image.src = 'assets/images/no-image.png';
+    image.alt = 'No image selected';
+  }
 }
 
 function changeVolume(){
 
-const controlVolume = document.getElementById('volume');
+  const controlVolume = document.getElementById('volume');
 
-//change volume
-const audio = document.getElementsByTagName('audio')[0];
-audio.volume = controlVolume.value/100;
+  //change volume
+  const audio = document.getElementsByTagName('audio')[0];
+  audio.volume = controlVolume.value/100;
 
-// change icon
-const volumeIcon = document.querySelector('#volume-controls img');
+  // change icon
+  const volumeIcon = document.querySelector('#volume-controls img');
 
-switch(true){
-  case (controlVolume.value == 0):
-    volumeIcon.src = "assets/icons/volume-level-0.svg";
-    volumeIcon.alt = "Volume level 0";
-    break;
-  case (1 <= controlVolume.value && controlVolume.value < 33):
-    volumeIcon.src = "assets/icons/volume-level-1.svg";
-    volumeIcon.alt = "Volume level 1";
-    break;
-  case (33 <= controlVolume.value && controlVolume.value < 67):
-    volumeIcon.src = "assets/icons/volume-level-2.svg";
-    volumeIcon.alt = "Volume level 2";
-    break;
-  case (67 <= controlVolume.value):
-    volumeIcon.src = "assets/icons/volume-level-3.svg";
-    volumeIcon.alt = "Volume level 3";
-    break;
-}
+  switch(true){
+    case (controlVolume.value == 0):
+      volumeIcon.src = "assets/icons/volume-level-0.svg";
+      volumeIcon.alt = "Volume level 0";
+      break;
+    case (1 <= controlVolume.value && controlVolume.value < 33):
+      volumeIcon.src = "assets/icons/volume-level-1.svg";
+      volumeIcon.alt = "Volume level 1";
+      break;
+    case (33 <= controlVolume.value && controlVolume.value < 67):
+      volumeIcon.src = "assets/icons/volume-level-2.svg";
+      volumeIcon.alt = "Volume level 2";
+      break;
+    case (67 <= controlVolume.value):
+      volumeIcon.src = "assets/icons/volume-level-3.svg";
+      volumeIcon.alt = "Volume level 3";
+      break;
+  }
 }
 
 function playSound(){
